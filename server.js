6896bab60cadc4e20c8a78aa0216efcae7496a70
shailesh.patel@atozinfoway.com <shailesh.patel@atozinfoway.com>
2024-08-20 15:26:44 +0530

    changes

--- server.js
+++ server.js
@@ -57,9 +57,9 @@ io.on("connection", (socket) => {
     io.to(data.to).emit("callAccepted", data.signal);
   });
 
-  socket.on("endCall", () => {
+  socket.on("endCall", (data) => {
     io.to(data.to).emit('callEnded');     
-	io.to(data.from).emit('callEnded');
+    io.to(data.from).emit('callEnded');
   });
 });
 
