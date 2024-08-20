227a94bb04f954f1699a7d8eb5074464c90c84cb
shailesh.patel@atozinfoway.com <shailesh.patel@atozinfoway.com>
2024-08-20 14:27:30 +0530

    user call end changes

--- server.js
+++ server.js
@@ -32,35 +32,47 @@ const http = require("http");
 const app = express();
 const server = http.createServer(app);
 const io = require("socket.io")(server, {
-  cors: {
-    origin: "http://localhost:3000",
-    methods: ["GET", "POST"]
-  }
+	cors: {
+		origin: "http://localhost:3000",
+		methods: ["GET", "POST"]
+	}
 });
+const clients = {};
 
 io.on("connection", (socket) => {
-  socket.emit("me", socket.id);
+	const clientId = socket.id;
+	clients[clientId] = socket;
 
-  socket.on("disconnect", () => {
-    socket.broadcast.emit("callEnded");
-  });
+	socket.on("disconnect", () => {
+		delete clients[clientId];
+	});
 
-  socket.on("callUser", (data) => {
-    io.to(data.userToCall).emit("callUser", {
-      signal: data.signalData,
-      from: data.from,
-      name: data.name
-    });
-  });
+	socket.on("callUser", (data) => {
+		const callerId = data.from;
+		const calleeId = data.userToCall;
 
-  socket.on("answerCall", (data) => {
-    io.to(data.to).emit("callAccepted", data.signal);
-  });
+		if (clients[calleeId]) {
+			clients[calleeId].emit("callUser", { signal: data.signalData, from: callerId, name: data.name });
+		}
+	});
 
-  socket.on("endCall", () => {
-    io.to(data.to).emit('call-ended');     
-	io.to(data.from).emit('call-ended');
-  });
+	socket.on("hangUp", (data) => {
+		const calleeId = data.to;
+
+		if (clients[calleeId]) {
+			clients[calleeId].emit("call-ended");
+		}
+	});
+
+
+	socket.on("answerCall", (data) => {
+		io.to(data.to).emit("callAccepted", data.signal);
+	});
+
+	socket.on("endCall", () => {
+		io.to(data.to).emit('call-ended');
+		io.to(data.from).emit('call-ended');
+	});
 });
 
 server.listen(5000, () => console.log("server is running on port 5000"));
