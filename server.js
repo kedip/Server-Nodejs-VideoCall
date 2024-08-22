82ef7c97a6e3d1291c5391f0bf618e67ffdbc8da
shailesh.patel@atozinfoway.com <shailesh.patel@atozinfoway.com>
2024-08-22 12:41:51 +0530

    new code

--- server.js
+++ server.js
@@ -53,13 +53,8 @@ io.on("connection", (socket) => {
     });
   });
 
-  // Handle call acceptance
   socket.on("answerCall", (data) => {
-    io.to(data.to).emit("callAccepted", {
-      signal: data.signal,
-      from: data.from, // Include the caller's ID
-      name: data.name // Include the caller's name
-    });
+    io.to(data.to).emit("callAccepted", data.signal);
   });
 
   socket.on("endCall", (data) => {
